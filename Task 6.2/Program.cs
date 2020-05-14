2657e97d639c310318eaa0ad60c817270a8fec74
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-14 23:05:44 +0300

    .

--- Task 6.2/Program.cs
+++ Task 6.2/Program.cs
@@ -10,6 +10,29 @@ namespace Task_6._2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int[] mass = new int[10];
+                int count = 0;
+                Random r = new Random();
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    mass[i] = r.Next(1, 40);
+                    Console.Write(mass[i] + "  ");
+                }
+                Console.WriteLine();
+
+                for (int i = 0; i < mass.Length - 1; i++)
+                {
+                    if (mass[i] < mass[i + 1]) count += 1;
+                }
+                Console.WriteLine(count);
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
